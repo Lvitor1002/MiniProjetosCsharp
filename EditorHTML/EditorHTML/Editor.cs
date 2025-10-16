b36012f7a30b3aa1dfd08e22106baaf8cea30ea8
Luiz Vitor <luizpierinovitor@gmail.com>
2025-10-16 20:46:12 -0300

    Feat: Nova classe editor

---
+++ EditorHTML/EditorHTML/Editor.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System;
+using System.Text;
+
+namespace EditorHTML
+{
+    public static class Editor
+    {
+        public static void Mostrar()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO EDITOR");
+            Console.WriteLine("-----------");
+            Iniciar();
+        }
+        public static void Iniciar()
+        {
+            //Quando o texto possui muitas linhas o ideal é usar o StringBuilder para melhorar a performance
+            var sb = new StringBuilder();
+            ConsoleKey key;
+            do
+            {
+                sb.Append(Console.ReadLine());
+                sb.Append(Environment.NewLine);
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.Clear();
+            Console.WriteLine("-----------");
+            Console.Write("Deseja salvar o arquivo? ");
+        }
+    }
+}
--- EditorHTML/EditorHTML/Menu.cs
+++ EditorHTML/EditorHTML/Menu.cs
@@ -9,8 +9,64 @@ namespace EditorHTML
             Console.BackgroundColor = ConsoleColor.Yellow;
             Console.ForegroundColor = ConsoleColor.Black;
             DesenharTela();
+
+            short opcao;
+            while (true)
+            {
+                ExibirOpcoes();
+                string entrada = Console.ReadLine().Trim();
+                if(!short.TryParse(entrada, out opcao))
+                {
+                    Console.Clear();
+                    Console.SetCursorPosition(10, 11);
+                    Console.Write("Opção inválida!");
+                    continue;
+                }
+                break;
+            }
+            ManipularOpcaoMenu(opcao);
         }
-        public static void DesenharTela()
+        
+
+        private static void ManipularOpcaoMenu(short opcao)
+        {
+            switch (opcao)
+            {
+                case 1:
+                    Console.Clear();
+                    Editor.Mostrar(); 
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Vizualizar"); 
+                    break;
+
+                case 0:
+                    Console.Clear();
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+        private static void ExibirOpcoes()
+        {
+            Console.SetCursorPosition(3, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("============");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo Arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+
+        private static void DesenharTela()
         {
             LinhasHorizontais();
             LinhasVerticais();
@@ -39,5 +95,6 @@ namespace EditorHTML
                 Console.Write("\n");
             }
         }
+        
     }
 }
