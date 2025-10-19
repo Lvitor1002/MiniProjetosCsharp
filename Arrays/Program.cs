52996eb196392ace59d976a93cbcab831c18f388
Luiz Vitor <luizpierinovitor@gmail.com>
2025-10-19 16:20:16 -0300

    Feat: Arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Arrays;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var meuArray = new int[5] {0,1,2,3,4};
+
+            Console.WriteLine(meuArray[0]);
+            Console.WriteLine(meuArray[1]);
+            Console.WriteLine(meuArray[2]);
+            Console.WriteLine(meuArray[3]);
+            Console.WriteLine(meuArray[4]);
+
+            Console.WriteLine("\n---===---===---===---===---===---===---===---===---===---===---===---===\n");
+
+
+
+
+
+            //Use for quando precisar de controle total sobre o índice;
+            for (var index = 0; index < meuArray.Length; index++) 
+                Console.Write($"| {meuArray[index]} ");
+
+
+
+
+
+
+            Console.WriteLine("\n\n---===---===---===---===---===---===---===---===---===---===---===---===\n");
+
+
+
+
+
+            Console.WriteLine($"Array: {string.Join(", ",meuArray)}.\n");
+
+
+
+
+
+            Console.WriteLine("\n---===---===---===---===---===---===---===---===---===---===---===---===\n");
+
+
+
+
+
+            //use foreach quando quiser apenas percorrer todos os elementos de forma simples e legível.
+            foreach (var item in meuArray)
+                Console.WriteLine(item);
+
+
+
+
+
+            Console.WriteLine("\n---===---===---===---===---===---===---===---===---===---===---===---===\n");
+
+
+
+
+            var funcionarios = new Funcionario[5];
+            funcionarios[0] = new Funcionario() 
+            {
+                Id = 12, 
+                Nome = "Luiz"
+            };
+
+            foreach (var funcionario in funcionarios)
+                Console.WriteLine($"Id: {funcionario.Id}\nNome: {funcionario.Nome}\n-=-=-=-=-=");
+
+
+
+
+            Console.WriteLine("\n---===---===---===---===---===---===---===---===---===---===---===---===\n");
+
+        }
+    }
+}
